80324fcb15e8dce407b6b9d6c130f90bba7d6d81
Dhruvi2411 <mangaroliyadhruvi24@gmail.com>
2026-09-21 12:15:04 +0530

    Added classes

---
+++ src/com/company/test/TestUtils.groovy
@@ -0,0 +1,7 @@
+package com.company.test
+
+class TestUtils{
+    def buildImage(String imagename) {
+        echo "Building image: ${imagename}...."
+    }
+}
---
+++ vars/testBuild.groovy
@@ -0,0 +1,7 @@
+import com.company.test.TestUtils
+
+def call(String imagename) {
+    def t1 = new TestUtils()
+
+    t1.testImage(imagename)
+}
